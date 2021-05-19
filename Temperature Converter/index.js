cf58d1c6894f950324a90c30bf49ebe5161cf31e
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-19 13:26:03 +0600

    Add files via upload

---
+++ Temperature Converter/index.js
@@ -0,0 +1,15 @@
+var Fahrenheit = parseFloat(prompt("please giben the Fahrenheit number:  "));
+
+var Celsius = (5/9) * (Fahrenheit - 32);
+document.write("Celsius is: "+Celsius);
+
+document.write("<br/>");
+document.write("<br/>");
+
+
+
+var Celsius  = parseFloat(prompt("please giben the Celsius  number:  "));
+
+var Fahrenheit = (9/5) * Celsius + 32;
+document.write("Fahrenheit is: "+Fahrenheit);
+
