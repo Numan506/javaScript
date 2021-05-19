2c36010d3c84416900efddc2c6dbc0ff3be28be9
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-19 13:07:51 +0600

    Add files via upload

---
+++ Area of  shapes/index.js
@@ -0,0 +1,19 @@
+var base =  parseFloat (prompt("Please giben the Base Number: "));
+var height = parseFloat( prompt("Please giben the Height Number: "));
+var radius = parseFloat( prompt("Please giben the Radius Number: "));
+var length_of_side = parseFloat( prompt("Please giben the length of side Number: "));
+
+var area = base * height;
+document.write("Area is : "+area +"<br/>");
+
+
+var Triangle = 09.5 * base * height ;
+document.write("Triangle is : "+Triangle  +"<br/>");
+
+
+var Circle = 3.1416 * radius * radius;
+document.write("Circle is : "+Circle +"<br/>");
+
+
+var length_of_side =  length_of_side * length_of_side;
+document.write("length of side is : "+length_of_side);
---
+++ Make calculator/index.js
@@ -0,0 +1,18 @@
+var num1 = parseInt (prompt("pease given the num1: "))
+var num2 = parseInt (prompt("pease given the num2: "))
+
+var result;
+result = num1+num2;
+document.write(num1 + " + " + num2 + " = " + result +"<br/>");
+
+result = num1-num2;
+document.write(num1 + " - " + num2 + " = " + result +"<br/>");
+
+result = num1/num2;
+document.write(num1 + " / " + num2 + " = " + result +"<br/>");
+
+result = num1*num2;
+document.write(num1 + " * " + num2 + " = " + result +"<br/>");
+
+result = num1%num2;
+document.write(num1 + " % " + num2 + " = " + result +"<br/>");
