3d79899d89f118088be56652dd9aa76fbdf81182
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-23 16:36:16 +0600

    Add files via upload

---
+++ Onclick event/index.js
@@ -0,0 +1,10 @@
+function button1(){
+    var myVar1= document.querySelector("#b1");
+    myVar1.innerHTML="hi! My Name is Numan.I lives in Dhaka,and i study at CSE in Bsc"
+}
+
+function button2(){
+    var myVar1= document.querySelector("#b1");
+    myVar1.innerHTML=" my favourite hobby is playing. I loves play Football"
+
+}
