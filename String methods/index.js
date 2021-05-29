1fe27fd2f7a5d0b5c5636db43b2739e4566bec36
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-29 15:50:23 +0600

    Add files via upload

---
+++ String methods/index.js
@@ -0,0 +1,11 @@
+
+const name = "The name of my country is Bangladesh";
+
+// ............use StartsWith............
+console.log(name.startsWith("The"));
+
+// ............use StartsWith............
+console.log(name.endsWith("Bangladesh"));
+
+// ............use includes............
+console.log(name.includes("Bangladesh"));
